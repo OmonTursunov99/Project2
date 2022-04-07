a161a0c835664287629e459b55bab380aca7f401
OmonTursunov99 <94193911+OmonTursunov99@users.noreply.github.com>
2022-04-07 19:42:22 +0300

    slider-swiper

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,6 @@ let viewCustomers = document.querySelector('#viewCustomers');
 let mainHeaderNavbar = document.querySelector('.main-header-navbar');
 let mobileButton = document.querySelectorAll('.mobileButton');
 let mainSliderItems = document.querySelectorAll('.main-slider-item');
-const SLIDER_CLASSES = [];
 
 let spinnerStop = () => {
     mainSpinner.style.display = 'flex';
@@ -30,12 +29,10 @@ dropdownItemsGroup.forEach(item => {
 inputDarkMode.onclick = function () {
     if(inputDarkMode.checked) {
         mainSpinner.classList.remove('dark');
-        spinnerStop();
         mainTag.classList.add('dark-mode');
         inputDarkMode.parentElement.classList.add('active');
     } else {
         mainSpinner.classList.add('dark');
-        spinnerStop();
         mainTag.classList.remove('dark-mode');
         inputDarkMode.parentElement.classList.remove('active');
 
@@ -49,36 +46,43 @@ mobileButton.forEach(item => {
         mainHeaderNavbar.classList.toggle('active');
     }
 })
+
+let slidesPerView = 5
 window.onresize = function () {
-    if (window.innerWidth > 939) mainHeaderNavbar.classList.remove('active');
+    let size = window.innerWidth
+    if ( size > 939) mainHeaderNavbar.classList.remove('active');
+    if (size < 1440 && size > 1160) slidesPerView = 4
 };
-let checkSliderClasses = () => {
-    mainSliderItems.forEach(item => {
-        SLIDER_CLASSES.push(item.className);
-    })
-}
-let changeSlideClasses = () => {
-    let a = SLIDER_CLASSES.splice(0, 1);
-    SLIDER_CLASSES.push(a[0]);
-    console.log(SLIDER_CLASSES)
-}
-let sliderControllerNext = () => {
-    changeSlideClasses();
-    let slideItems = document.querySelectorAll('.main-slider-item');
-    slideItems.forEach((item, index) => {
-        let slideLength = mainSliderItems.length;
-        if(index === slideLength - 1) item.classList = SLIDER_CLASSES[0];
-        else item.classList = SLIDER_CLASSES[index + 1];
-    });
-}
-checkSliderClasses();
-let slideInterval = setInterval(() => {
-    sliderControllerNext();
-}, 2000);
-// clearInterval(slideInterval)
-let stopInterval = () => {
-    clearInterval(slideInterval)
-}
+const swiper = new Swiper('.swiper', {
+    slidesPerView: slidesPerView,
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: {
+        delay: 2000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1440: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        },
+    }
+});
 
 
 
